7a1c327e6367e8d0c6e0f9ec7ef74560ca6f742c
Axel Padilla <adpadillar25@gmail.com>
2023-11-08 00:50:12 +0000

    update ai-server

--- src/generate.py
+++ src/generate.py
@@ -1,9 +1,13 @@
 # main.py
 import torch
+import cv2
+import numpy as np
 from torch import autocast
 from diffusers import StableDiffusionPipeline
 from io import BytesIO
 import base64
+from super_image import EdsrModel, ImageLoader
+from PIL import Image
 
 
 MODEL = "CompVis/stable-diffusion-v1-4"
@@ -14,6 +18,7 @@ pipe = None
 def prepPipeline():
     global warm
     global pipe
+    global u_model
 
     if not warm:
         pipe = StableDiffusionPipeline.from_pretrained(
@@ -22,6 +27,8 @@ def prepPipeline():
         pipe.to("cuda")
         pipe.safety_checker = None
         pipe.requires_safety_checker = False
+
+        u_model = EdsrModel.from_pretrained('eugenesiow/edsr-base', scale=4)
 
         warm = True
 
@@ -35,9 +42,19 @@ def generate(prompt: str, height: int, width: int, steps: int):
         output = pipe(prompt, height=height, width=width,
                       num_inference_steps=steps)
     for image in output.images:
-        # base64 encode image
+        input_1 = ImageLoader.load_image(image)
+        preds_1 = u_model(input_1)
+        
+        ImageLoader.save_image(preds_1, "./out.png")
+        
+        im = cv2.imread("./out.png")
+        dst = cv2.fastNlMeansDenoisingColored(im, None, 11, 6, 7, 21)
+        cv2.imwrite("./out.png", dst)
+
+        im = Image.open("./out.png")
+
         buffered = BytesIO()
-        image.save(buffered, format="JPEG")
+        im.save(buffered, format="JPEG")
         img_str = base64.b64encode(buffered.getvalue())
         base64_img_str = img_str.decode("utf-8")
 
